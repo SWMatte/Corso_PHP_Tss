4b2c671237cfa981ab0512f296cf5549e1635b7a
Matteo P <cambogia.daicazzo@gmail.com>
2023-02-06 12:53:07 +0100

    fine lezione 6/02

--- form_in_php/log-in-response.php
+++ form_in_php/log-in-response.php
@@ -1,23 +1,21 @@
-<h1> Sono la risposta (RESPONSE)</h1>
-<?php
+ <?php
+/*1)come passiamo i dati: via get 
+2) variabile da ottenere cioe la stringa "email" 
+che vogliamo ottenere dal get
+3) è una costante, che valida l'email.
 
-echo "<pre>";
-echo "sono get ";
-print_r($_GET);
-echo "<pre>";
+*/
+ 
 
-echo "<pre>";
-echo "sono post ";
-print_r($_POST);
-echo "<pre>";
+$test= filter_input(INPUT_GET,"email",FILTER_VALIDATE_EMAIL);
 
-echo "La tua email è <br>";
-echo "<strong>".$_POST['email']."</strong>";
-
-
-
+if($test==false){
+    echo "\n la mail non è valida\n";
+} else{
+    echo "la mail è valida: $test";
+}
 
 
 
 
-?>
+ ?>
