2dbdeeaa3c7d571b9454d4cd90e0b60cb2fb7e0a
Matteo P <cambogia.daicazzo@gmail.com>
2023-02-22 12:05:59 +0100

    users-con-classe-invalid

--- form_in_php/create-user.php
+++ form_in_php/create-user.php
@@ -1,3 +1,18 @@
+<?php
+/* rappresenta l'ambiente dove gira lo script è una variabile super globale
+l'indice lo prendi mandando a schermo solo il server e cercando con ctrl u la richiesta post
+
+
+*/
+//  print_r($_SERVER['REQUEST_METHOD']);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  echo 'dati inviati, ora li devo controllare';
+} else {
+  echo 'non devo fare nulla';
+}
+?>
+
+
 <!doctype html>
 <html lang="en">
 
@@ -5,8 +20,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Bootstrap demo</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 
 <body>
@@ -21,24 +35,37 @@
       </div>
       <div class="col-sm-8">
 
-        <form class="mt-1 mt-md-5" action="register-user.php" method="POST">
+        <form class="mt-1 mt-md-5" action="create-user.php" method="POST">
 
           <div class="mb-3">
             <label for="first_name" class="form-label">nome</label>
-            <input type="text" class="form-control" name="first_name" id="first_name">
+            <input type="text" class="form-control " name="first_name" id="first_name">
+            <div class="invalid-feedback">
+              errore
+            </div>
           </div>
 
           <div class="mb-3">
             <label for="last_name" class="form-label">cognome</label>
             <input type="text" class="form-control" name="last_name" id="last_name">
+            <div class="invalid-feedback">
+              errore
+            </div>
           </div>
+
           <div class="mb-3">
             <label for="birthday" class="form-label">data di nascita</label>
             <input type="date" class="form-control" name="birthday" id="birthday">
+            <div class="invalid-feedback">
+              errore
+            </div>
           </div>
           <div class="mb-3">
             <label for="birth_place" class="form-label">luogo di nascita</label>
             <input type="text" class="form-control" name="birth_place" id="birth_place">
+            <div class="invalid-feedback">
+              errore
+            </div>
           </div>
 
 
@@ -46,26 +73,35 @@
           <div class="mb-3">
             <span>Genere</span>
             <div class="form-check">
-              <input class="form-check-input" type="radio" name="gender" id="gender_M">
+              <input class="form-check-input " type="radio" name="gender" id="gender_M">
+
               <label class="form-check-label" for="gender_M">
                 Maschile
               </label>
             </div>
             <div class="form-check">
-              <input class="form-check-input" type="radio" name="gender" id="gender_F" checked>
+              <input class="form-check-input" type="radio" name="gender" id="gender_F">
               <label class="form-check-label" for="gender_F">
                 Femminile
               </label>
-
+              <div class="invalid-feedback">
+                errore
+              </div>
             </div>
           </div>
           <div class="mb-3">
             <label for="username" class="form-label">Nome utente</label>
             <input type="text" class="form-control" name="username" id="username">
+            <div class="invalid-feedback">
+              errore
+            </div>
           </div>
           <div class="mb-3">
             <label for="password" class="form-label">Password</label>
             <input type="password" class="form-control" name="password" id="password">
+            <div class="invalid-feedback">
+              errore
+            </div>
           </div>
 
 
@@ -79,9 +115,7 @@
   </main>
 
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 
 </html>
